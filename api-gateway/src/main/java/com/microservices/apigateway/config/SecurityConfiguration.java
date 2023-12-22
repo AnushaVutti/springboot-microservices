bb84d98b2427de8dfb5e9b38f7a0673ec80793ba
AnushaVutti <anushavutti1@gmail.com>
2023-12-22 12:23:54 -0500

    Enabled security in api gw and basic auth in eureka servre

--- api-gateway/src/main/java/com/microservices/apigateway/config/SecurityConfiguration.java
+++ api-gateway/src/main/java/com/microservices/apigateway/config/SecurityConfiguration.java
@@ -1,44 +1,50 @@
-//package com.microservices.apigateway.config;
+package com.microservices.apigateway.config;
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.server.servlet.OAuth2AuthorizationServerJwtAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfiguration{
 //
-//import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
-//import org.springframework.boot.autoconfigure.security.oauth2.server.servlet.OAuth2AuthorizationServerJwtAutoConfiguration;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.config.web.server.ServerHttpSecurity;
-//import org.springframework.security.oauth2.jwt.Jwt;
-//import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-//import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.server.SecurityWebFilterChain;
-//import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
-//
-//@Configuration
-//public class SecurityConfiguration {
-////
-//    @Bean
-//    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity httpSecurity) throws Exception {
-//////        return httpSecurity.cors(AbstractHttpConfigurer::disable)
-//////                .csrf(AbstractHttpConfigurer::disable)
-//////                .authorizeHttpRequests(httpRequests -> httpRequests.requestMatchers("/eureka/**").permitAll().anyRequest().authenticated())
-//////                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//////                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()))
-//////                .build();
-//         return httpSecurity.authorizeExchange(exchage -> exchage
-//                 .pathMatchers("/eureka/**")
-//                 .permitAll()
-//                 .anyExchange()
-//                 .authenticated())
-//                 .exceptionHandling(exceptionHandling -> exceptionHandling
-//                 .authenticationEntryPoint(new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)))
-//                 //.oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt)
-//                 .oauth2Login(Customizer.withDefaults())
-//                 //.oauth2ResourceServer(http.oauth2ResourceServer().jwt())
+    @Bean
+    public SecurityWebFilterChain  securityFilterChain(ServerHttpSecurity httpSecurity) throws Exception {
+////        return httpSecurity.cors(AbstractHttpConfigurer::disable)
+////                .csrf(AbstractHttpConfigurer::disable)
+////                .authorizeHttpRequests(httpRequests -> httpRequests.requestMatchers("/eureka/**").permitAll().anyRequest().authenticated())
+////                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+////                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()))
+////                .build();
+        return httpSecurity.csrf(csrf -> csrf.disable()).authorizeExchange(exchage -> exchage
+                        .pathMatchers("/eureka/**").permitAll())
+                .authorizeExchange(exchange -> exchange.anyExchange().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(withDefaults())
+                ).build();
+    }
 //                 .build();
-//    }
-//
-//}
+//        return httpSecurity.csrf(csrf->csrf.disable())
+//                .authorizeHttpRequests(auth->auth.requestMatchers("/eureka/**").permitAll())
+//                .authorizeHttpRequests(auth->auth.anyRequest().authenticated())
+//                .oauth2ResourceServer(oauth2 -> oauth2
+//                        .jwt(withDefaults())
+//                .build();
+
+
+}
--- discovery-server/src/main/java/com/microservices/discoveryserver/config/SecurityConfiguration.java
+++ discovery-server/src/main/java/com/microservices/discoveryserver/config/SecurityConfiguration.java
@@ -1,4 +1,58 @@
 package com.microservices.discoveryserver.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
+    @Value("${eureka.username}")
+    private String username;
+    @Value("${eureka.password}")
+    private String password;
+//    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+//        System.out.println("In configure of eureka");
+//        authenticationManagerBuilder.inMemoryAuthentication()
+//                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+//                .withUser("eureka").password("password")
+//                .authorities("USER");
+//    }
+//    public void configure(HttpSecurity httpSecurity) throws Exception {
+//        System.out.println("In configure of eureka -> 2");
+//        httpSecurity.csrf(csrf -> csrf.disable()).authorizeRequests().anyRequest()
+//                .authenticated().and().httpBasic(Customizer.withDefaults());
+//    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf->csrf.disable()).authorizeHttpRequests((authorize) -> {
+            authorize.anyRequest().authenticated();
+        }).httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+@Bean
+public UserDetailsService userDetailsService() {
+
+    UserDetails admin = User.builder().username("eureka").password(passwordEncoder().encode("password")).roles("ADMIN")
+            .build();
+    return new InMemoryUserDetailsManager(admin);
+}
+
 }
