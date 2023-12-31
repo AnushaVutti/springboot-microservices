44e9aa08db443e096b0d897cbfd830abeec647f0
AnushaVutti <anushavutti1@gmail.com>
2023-12-30 22:00:35 -0500

    added notification service dockerization and monitoring

--- orderservice/src/main/java/com/microservices/orderservice/controller/OrderController.java
+++ orderservice/src/main/java/com/microservices/orderservice/controller/OrderController.java
@@ -2,10 +2,15 @@ package com.microservices.orderservice.controller;
 
 import com.microservices.orderservice.dto.OrderRequest;
 import com.microservices.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.weaver.ast.Or;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -14,8 +19,13 @@ public class OrderController {
     private final OrderService orderService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequest orderRequest) {
-        orderService.placeOrder(orderRequest);
-        return "Order placed successfully";
+    @CircuitBreaker(name="inventory", fallbackMethod="fallbackMethod")
+    @TimeLimiter(name="inventory")
+    @Retry(name="inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
+        return CompletableFuture.supplyAsync(()->orderService.placeOrder(orderRequest));
+    }
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return CompletableFuture.supplyAsync(()->"Oops! something went wrong!! So, please try ordering after sometime");
     }
 }
